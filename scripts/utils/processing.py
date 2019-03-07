55e3687c1cf4242b389cb905565102f99ae2f7cf
tharangni <tharangni@hotmail.com>
2019-03-07 02:11:29 +0100

    changing w_n

--- scripts/utils/processing.py
+++ scripts/utils/processing.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import numpy as np
 import pandas as pd
 import igraph as ig
@@ -71,7 +72,7 @@ def lookup_table(filename, subset):
 # # N and labels_dict have to be present globally! (list of all the labels)
 # # labels_dict because I will keep accessing it for each document
 # order_label_mapping = generate_label_vector(N)
-def generate_label_vector(N_all_nodes):
+def generate_order_mapping(N_all_nodes):
 
 	order_mapping = {}
 	sorted_nodes = sorted(N_all_nodes)
@@ -81,3 +82,51 @@ def generate_label_vector(N_all_nodes):
 			order_mapping[each_node] = i+1
 
 	return order_mapping
+
+
+def list2tensor(inp_list):
+	'''
+	converts list to tensor
+	'''
+	list_tensors = list(map(torch.Tensor, inp_list))
+	out_tensor = torch.stack(list_tensors, dim=0)
+
+	return out_tensor
+
+
+mem = Memory("./../../mycache")
+@mem.cache
+def generate_wn(N_all_nodes, n):
+	'''
+	Creates unique parameters for all nodes. 
+	Acts like a dictionary
+	'''
+	list_all = []
+	for _ in tqdm(range(len(N_all_nodes))):
+		c = torch.Tensor(n,).uniform_(0, 1)
+		c = list(c.numpy())
+		if c not in list_all:
+			list_all.append(c)
+
+	wn_tensors = list2tensor(list_all)
+	
+	return wn_tensors
+
+
+def generate_binary_yin(N_all_nodes):
+	'''
+	Alternate method to generate y_in values. If a node n belongs to an 
+	instance i, it accesses the respective 16-bit binary {-1, +1} representation 
+	of that node-number (from order_mapping fn) as the respective y_in.
+	'''
+	all_16 = []
+	for i in tqdm(range(1, len(N_all_nodes)+1)):
+		bin_rep = bin(i)[2:]
+		rep_16 = '{:016d}'.format(int(bin_rep))
+		list_16 = list(map(int, rep_16))
+		all_16.append(list_16)
+
+	t_16 = list2tensor(all_16)
+	y_in_dash = torch.from_numpy(np.where(t_16.numpy() > 0, 1, -1)).float()
+	
+	return y_in_dash
